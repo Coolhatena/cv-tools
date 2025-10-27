acee28ae536e049e379e461745517b09d05a191c
Coolhatena <ingsc.geffroy@gmail.com>
2025-10-26 19:07:09 -0700

    Use folder name as prefix & move extension to a variable

--- renameAndCopy.py
+++ renameAndCopy.py
@@ -6,24 +6,27 @@ import random
 import string
 
 source_folders = [
-    'small0/',
-    'small1/'
+    'cable/',
+    'caja/',
+    'manual/',
+    'teclado/',
+    'teclado_bolsa/',
 ]
-target_folder = 'images_9ago/'
-prefix = ''
+target_folder = 'images/'
+extension = '.jpg' # image type
 
 # Create target folder if it does not exits
 os.makedirs(target_folder, exist_ok=True)
 
-def random_name():
+def random_name(prefix):
     return prefix + ''.join(random.choices(string.ascii_letters + string.digits, k=6))
 
 
 for folder in source_folders:
     for img_file in os.listdir(folder):
         file_path = os.path.join(folder, img_file)
-        if os.path.isfile(file_path) and img_file.lower().endswith('.png'):
-            new_name = random_name() + '.png'
+        if os.path.isfile(file_path) and img_file.lower().endswith(extension):
+            new_name = random_name(folder[:-1]) + extension
             target_path = os.path.join(target_folder, new_name)
             shutil.copy2(file_path, target_path)
             print(f'Copied: {file_path} -> {target_path}')
